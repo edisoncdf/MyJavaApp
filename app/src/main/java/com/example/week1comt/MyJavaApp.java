200c39ca32bb630513ed85b1e8cd8f75a750a88b
edisoncdf <2100483C@tp.edu.sg>
2021-04-24 20:13:30 +0800

    v1.0 ComT Week 1 Backup

---
+++ app/src/main/java/com/example/week1comt/MyJavaApp.java
@@ -0,0 +1,20 @@
+package com.example.week1comt;
+
+public class MyJavaApp {
+    public static void main(String[] args) {
+        double lengthoftable=1.6;
+        String flowername="Angelica";
+        double iphoneprice=1199.99;
+        int studentage=17;
+        char chemgrade='B';
+        boolean rainstopped= true;
+        System.out.println("length of table is "+lengthoftable+"m");
+        System.out.println("the name of the flower is "+flowername);
+        System.out.println("the price of an iPhone is $"+iphoneprice);
+        System.out.println("the age of a student is "+studentage);
+        System.out.println("the grade of chemistry is "+chemgrade);
+        System.out.println("it is "+rainstopped+" that rain has stopped");
+
+
+    }
+}
---
+++ app/src/main/java/com/example/week1comt/Week1ComTLesson.java
@@ -0,0 +1,12 @@
+package com.example.week1comt;
+
+public class Week1ComTLesson {
+    public static void main(String[] args) {
+        String name= "john";
+        System.out.println("my name is "+name);
+    }
+
+
+
+
+}
