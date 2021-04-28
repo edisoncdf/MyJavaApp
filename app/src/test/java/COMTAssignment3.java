3369c14463af3c41fdebffc41b4b3a182e8119e2
edisoncdf <2100483C@tp.edu.sg>
2021-04-28 15:58:28 +0800

    v1.1 ComT Week 1 Backup Added SOUT hello world Added COMT Assignment 1 Added COMT Assignment 2 Added COMT Assignment 3

---
+++ app/src/test/java/COMTAssignment3.java
@@ -0,0 +1,12 @@
+public class COMTAssignment3 {
+    public static void main(String[] args){
+int ar[]= {55,24,65,41,13,76,90};
+int i, x;
+for(i=0; i<ar.length; i++)
+{
+    x= ar[i];
+    if (x>=55)
+    System.out.println(x+" ");
+}
+    }
+}
