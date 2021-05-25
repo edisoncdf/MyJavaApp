2e22979235369e1574e69e2ecfdd4ba68011f335
edisoncdf <2100483C@tp.edu.sg>
2021-05-25 14:32:12 +0800

    v1.1 ComT Week 1 Backup Added SOUT hello world Added COMT Assignment 1 Added COMT Assignment 2 Added COMT Assignment 3 Added COMT Assignment 5 Added COMT Assignment 4 Added TestTriangle Modified COMT Assignment 4

--- app/src/test/java/COMTAssignment4.java
+++ app/src/test/java/COMTAssignment4.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 
 public class COMTAssignment4 {
-        static void calculatePrice () {
-            int ipadquantity;
-            int ipadprice = 1000;
-            System.out.println("Enter the number of iPads: ");
-            Scanner scanner = new Scanner(System.in);
-            ipadquantity = scanner.nextInt();
-            System.out.println("The Total Cost of the iPads is: $" + ipadprice * ipadquantity);
-        }
-
-    public static void main(String[] args) {
-        calculatePrice();
-    }
+    static int calculatePrice () {
+        int ipadquantity;
+        int ipadprice = 1000;
+        System.out.println("Enter the number of iPads: ");
+        Scanner scanner = new Scanner(System.in);
+        ipadquantity = scanner.nextInt();
+        int cost= ipadprice*ipadquantity;
+        return cost;
     }
 
-
+    public static void main(String[] args)
+    {
+        System.out.println("The Total Cost of the iPads is: $" + calculatePrice());
+    }
+}
 
